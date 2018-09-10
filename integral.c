ff423fcd09398f4e9e6487e23f5c534e7448961d
jvrobinho <jvmonteirorj@gmail.com>
2018-09-09 22:05:46 -0300

    Balanceamento de carga ehostfile para limitar os processos

--- integral.c
+++ integral.c
@@ -16,25 +16,56 @@ int main(int argc, char **argv){
     int source;             // remetente da integral
     int dest = 0;           // destino das integrais (nó 0)
     int tag = 200;          // tipo de mensagem (único)
-    MPI_Status status;
-
+    MPI_Status status;    
+    
+    int rem_n; //Remaining trapezoids
+    float rem_integral;
+    float rem_total;
+    float before_rem;
+    
     float calcula(float local_a, float local_b, int local_n, float h);
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);    
+       
+    rem_n = n % p;
+    //printf("Remaining trapezoids = %d\n", rem_n);
+
     h = (b - a) / n;
+    //printf("Altura = %f\n", h);
+
     local_n = n / p;
+    //printf("Local n = %d\n", local_n);
+
     local_a = a + my_rank * local_n * h;
+    //printf("Local a = %f\n", local_a);
+
     local_b = local_a + local_n * h;
+    //printf("Local b = %f\n\n", local_b);
+    
     integral = calcula(local_a, local_b, local_n, h);
+
+    if(rem_n && my_rank != 0){
+        //Each process will calculate ONE trapezoid.
+        rem_integral = calcula(local_a, local_b, 1, h);
+        //printf("Remainder trapezoids integral (rank %d) = %f\n\n", my_rank, rem_integral);    
+    }
+
 
     //Reduce all local integrals to the master process (rank 0) using MPI_SUM.
     total = integral;
     MPI_Reduce(&integral, &total, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
     
-    //Send all local integrals to the master process (rank 0) and sum into "total".
-    //Comment out the code above to use this snippet.
+    before_rem = total; //Area before summing the remainder trapezoids. 
 
+    //For debugging purposes.
+    rem_total = rem_integral;
+    MPI_Reduce(&rem_integral, &rem_total, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    //Send all local integrals to the master process (rank 0) and sum into "total".
+    //Choose either the code above or the one below.
+    
     // if (my_rank == 0){
     //     total = integral;
     //     for (source = 1; source < p; source++){
@@ -46,9 +77,27 @@ int main(int argc, char **argv){
     // else
     //     MPI_Send(&integral, 1, MPI_FLOAT, dest,
     //              tag, MPI_COMM_WORLD);
+   
 
-    if (my_rank == 0)
-        printf("Resultado do calculo: %f\n", total);
+    //Perform load balancing so each processor calculates only one trapezoid's area.
+    //By the end, all remaining trapezoids will have their area calculated.
+    if (my_rank == 0){
+        for (source = 1; source < p; source++){
+            MPI_Recv(&rem_integral, 1, MPI_FLOAT, source, tag, MPI_COMM_WORLD, &status);
+            total += rem_integral;
+        }
+    }
+    else
+        MPI_Send(&rem_integral, 1, MPI_FLOAT, dest, tag, MPI_COMM_WORLD);
+    
+    //Print results
+    if (my_rank == 0){
+        printf("**********************************************************\n");
+        printf("*Area excluindo trapezoides resultantes: %f        *\n", before_rem);
+        printf("*Numero de trapezoides resultantes: %d                    *\n", rem_n);
+        printf("*Area dos trapezoides resultantes: %f              *\n", rem_total);
+        printf("*Resultado final: %f                               *\n", total);
+    }
     MPI_Finalize();
 }
 
